769a193c626ac6a4c942ec2a0d01d15ee1a31179
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-02-05 00:10:37 +0530

    Create JavaConcepts.java

---
+++ JavaConcepts.java
@@ -0,0 +1,15 @@
+/*take user input*/
+package javaconcepts;
+import java.util.Scanner;
+public class JavaConcepts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter number");
+		int n = scan.nextInt();
+		System.out.println("The entered value is");
+		System.out.println(n);
+	}
+
+}
